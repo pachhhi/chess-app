0902f22516d497d53301ff19a2d91688731277ee
pachhhi <carrizopadua@gmail.com>
2024-04-15 02:41:39 -0300

    fix board

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,28 +2,28 @@
 
 import "../app/globals.css"
 import { Chess } from "chess.js"
-import { Chessboard, FEN, Markers } from "cm-chessboard";
+import { Chessboard, FEN } from "cm-chessboard";
 import { useEffect } from "react";
-import "/public/assets/chessboard.css"
+import "cm-chessboard/assets/chessboard.css"
 
 
 export default function Home() {
 
   useEffect(() => {
-    
+    console.log('rendering chessboard')
     const board = new Chessboard(document.getElementById("board"),{
-      
-      position: FEN.start
-      
+      position: "rn2k1r1/ppp1pp1p/3p2p1/5bn1/P7/2N2B2/1PPPPP2/2BNK1RR w Gkq - 4 11"
     });
 
-  },[]);
+    return () => {
+      board.destroy();
+    }
+
+  }, []);
 
   return (
     <main>
-      <div id="board" className="w-10 h-50">
-        
-      </div>
+      <div id="board" className="w-2/5 "></div>
     </main>
   );
 }
